d9a5dd8ec89dcf034abccce6c254e19de94efd59
Matthew Abbott <98388418+matthewJamesAbbott@users.noreply.github.com>
2022-08-19 13:42:36 +1000

    Add files via upload

--- Board.pas
+++ Board.pas
@@ -5,13 +5,26 @@ program Pascal-Chess;
 type
     Board = class
     private
-        array[8,8] of string;
+        board = array[8,8] of string;
 
     public
         constructor create();
-        procedure setSquare(x, y: integer);
+        procedure setSquare(x, y: integer; piece: string);
 
         function returnSquare(x, y): string;
 end;
 
+constuctor Board.create();
+begin
 
+end;
+
+procedure Board.setSquare(x, y: integer; piece: string);
+begin
+    board[x,y] := piece;
+end;
+
+function returnSquare(x, y): string;
+begin
+    returnSquare := board[x,y];
+end;
