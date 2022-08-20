66df562deed163e6721e73f480d277ab8f408d09
Matthew Abbott <98388418+matthewJamesAbbott@users.noreply.github.com>
2022-08-20 11:32:25 +1000

    Add files via upload

--- Game.pas
+++ Game.pas
@@ -3,42 +3,79 @@
 //
 
 {$mode objfpc}
-{$m+}
 
-
-program PascalChess;
+unit Game;
+interface
 
 uses
-    Board, MoveRecorder, Engine, Print, MoveCalculator
+    Board; //, MoveRecorder, Engine, Print, MoveCalculator;
 type
-
-    Game = Class (Print)
+    serverPort = array[0 .. 3] of char;
+    TGame =  Object
     private
         
     public
         constructor create();
         procedure setPlayerOne(name: string);
         procedure setPlayerTwo(name: string);
-        procedure initialiseBoard();
+        procedure initialiseBoard();{
         procedure initialiseBoardReverse();
         function recallMove(): boolean;
         procedure loadGame(fileName: string);
         function movePiece(xa, xb: integer; ca, cb: integer): boolean;
-        function engineMove(): boolean;
-        clientServerToggle, clientPort, playerSide = integer;
-        serverPort = array[0 .. 3] of char;
-        clientIP = string;
+        function engineMove(): boolean;}
 end;
 
 var
-    gameBoard: Board;
-    rec: MoveRecorder;
-    calc: MoveCalculator;
-    moveEngine: Engine;
+    gameBoard: TBoard;{
+    rec: TMoveRecorder;
+    calc: TMoveCalculator;
+    moveEngine: TEngine;}
+    clientServerToggle, clientPort, playerSide : integer;
+    clientIP : string;
 
-constructor Game.create();
+
+implementation
+
+constructor TGame.create();
+begin
+    gameBoard.create();
+end;
+
+procedure TGame.setPlayerOne(name: string);
 begin
 
 end;
 
+procedure TGame.setPlayerTwo(name: string);
+begin
 
+end;
+
+{
+load string representations of pieces into 2D array board black bottom
+take note that due to the way the terminal prints 0,0 in 
+the array is 7,0 on the board so this initialises 'White Left Rook'
+to square 8A
+}
+
+procedure TGame.initialiseBoard();
+begin
+
+    {
+    load white pieces into 2D array starting at 8A
+    finishing at 7H
+    }
+
+     gameBoard.setSquare(0,0,'White Left Rook');
+     gameBoard.setSquare(0,1,'White Left Knight');
+     gameBoard.setSquare(0,2,'White Left Bishop');
+     gameBoard.setSquare(0,3,'White King');
+     gameBoard.setSquare(0,4,'White Queen');
+     gameBoard.setSquare(0,5,'White Right Bishop');
+     gameBoard.setSquare(0,6,'White Right Knight');
+     gameBoard.setSquare(0,7,'White Right Rook');
+
+end;
+
+end.
